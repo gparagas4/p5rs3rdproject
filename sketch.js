1968e21519831c3f6e40fd2c575ade5e371ef643
Gustav Paragas <gep21b@fsu.edu>
2022-10-26 12:50:51 -0400

    background image insert

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+let Width = 5100
+let Height = 3300
+
+let img;
+
+function preload (){
+  img = loadImage(assests/background.jpg)
+}
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(CanvasWidth, CanvasHeight);
 }
 
 function draw() {
   background(125);
+  image (img, 0, 0)
 }
