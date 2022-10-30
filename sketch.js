6f141df52052560b12a56b75b6d2a145f0470f46
Gustav Paragas <gep21b@fsu.edu>
2022-10-30 15:48:10 -0400

    game update

--- sketch.js
+++ sketch.js
@@ -1,24 +1,41 @@
-let canvasWidth = 3000;
+let canvasWidth = 2175;
 let canvasHeight = 2000;
 let x = 0;
+let rectSize = 30;
+let goingForward = true;
 
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
+  rectMode(CENTER);
+
 }
 function draw() {
-
-background ("skyblue")
-  ellipse(1800, 55, 70)
-  fill("yellow")
+  background ("skyblue")
+  ellipse(1800, 45, 70)
+  color ("purple")
   noStroke()
+    
+
+  rect(2000, 100, 400, 35)
+
+  rect(100, 185, 400, 35)
+  if (x=canvasWidth) {
+    x++;
+  } else {
+    x--;
+  }
+
+  if (x == canvasWidth){
+ goingForward = false;
+  }
+  if (x== 0){
+    goingForward = true;
+  }
+
+    rect(x, canvasHeight/2, 30, 30)
+  }
 
 
-    rect(1800, 90, 250, 20)
-    floor.rotation = 2;
-    fill("purple")
-    noStroke()
+ 
 
-   
-  
-}
